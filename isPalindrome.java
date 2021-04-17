e7f6ee769c7a3d6facc18f47f8b7113e7840712a
VishalGhai <vghai2710@gmail.com>
2021-04-17 22:17:21 +0530

    String_Day 3

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,9 +1,9 @@
 public class isPalindrome {
-    int isPlaindrome(String S) {
+    static int isPalindrome(String S) {
         // code here
         return (new StringBuilder(S)).reverse().toString().equals(S)?1:0;
     }
     public static void main(String[] args) {
-        System.out.println("abba is palindrome : " + isPlaindrome("abba"));
+        System.out.println("abba is palindrome : " + isPalindrome("abba"));
     }
 }
